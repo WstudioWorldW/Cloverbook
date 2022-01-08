da9715eea3bfd528d16e80b5254d5a0fe7d928d7
Danil Sabitov <danil.snip2004@gmail.com>
2022-01-09 01:22:10 +0500

    v0.0.1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <layout />
+</template>
+
+<style lang="scss">
+
+@import 'assets/styles/fonts.css';
+@import 'assets/styles/variable.scss';
+@import 'assets/styles/all.min.css';
+
+</style>
+
+
+<script>
+
+import layout from './layouts/layout.vue'
+
+export default {
+    components: {
+        layout
+    }
+}
+
+</script>
