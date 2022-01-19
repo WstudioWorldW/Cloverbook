2764a2da08e5092e2b25cdb8fec534d44de972bd
Danil Sabitov <danil.snip2004@gmail.com>
2022-01-19 19:01:29 +0500

    deploy

---
+++ precache-manifest.dfe3835c1b440f4a7bc3e771b525ebda.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4486887e6058026b460c",
+    "url": "/Cloverbook/css/app.e7857aad.css"
+  },
+  {
+    "revision": "9f81d628ecf6c80f7ebf80e1ed455d92",
+    "url": "/Cloverbook/fonts/Comfortaa-Bold.9f81d628.ttf"
+  },
+  {
+    "revision": "7fc83210bd61acc3ec2d6b92e320dde7",
+    "url": "/Cloverbook/fonts/Comfortaa-Light.7fc83210.ttf"
+  },
+  {
+    "revision": "31aefe19554b85e0e5c1232daebc158e",
+    "url": "/Cloverbook/fonts/Comfortaa-Regular.31aefe19.ttf"
+  },
+  {
+    "revision": "05c475fabceebae1f9d40ba6711cb41a",
+    "url": "/Cloverbook/fonts/fa-brands-400.05c475fa.eot"
+  },
+  {
+    "revision": "5d875e6a4dcceea77fa7d4b8e1440982",
+    "url": "/Cloverbook/fonts/fa-brands-400.5d875e6a.woff"
+  },
+  {
+    "revision": "7c0be8b6640f024b6f4505161bf1bfd4",
+    "url": "/Cloverbook/fonts/fa-brands-400.7c0be8b6.woff2"
+  },
+  {
+    "revision": "8ca6e74eddaf8fce3cdcf8647b37cbf1",
+    "url": "/Cloverbook/fonts/fa-brands-400.8ca6e74e.ttf"
+  },
+  {
+    "revision": "6bd3ad9dbce9c232c66bfaee1e4c374f",
+    "url": "/Cloverbook/fonts/fa-duotone-900.6bd3ad9d.eot"
+  },
+  {
+    "revision": "7bc112bc25c528dc4bbfec047c69d338",
+    "url": "/Cloverbook/fonts/fa-duotone-900.7bc112bc.woff"
+  },
+  {
+    "revision": "c877e45db0614bba8e121510162149fc",
+    "url": "/Cloverbook/fonts/fa-duotone-900.c877e45d.woff2"
+  },
+  {
+    "revision": "d80e843337fcdf622266a3d6d0168991",
+    "url": "/Cloverbook/fonts/fa-duotone-900.d80e8433.ttf"
+  },
+  {
+    "revision": "0b4313e036624b29c8f651d0c9a5ecd8",
+    "url": "/Cloverbook/fonts/fa-light-300.0b4313e0.woff"
+  },
+  {
+    "revision": "0e0b8b741276d5220c847d0be486609e",
+    "url": "/Cloverbook/fonts/fa-light-300.0e0b8b74.ttf"
+  },
+  {
+    "revision": "3d8af27a47bbfab857f96dbcf710b087",
+    "url": "/Cloverbook/fonts/fa-light-300.3d8af27a.woff2"
+  },
+  {
+    "revision": "a624ea3f4dd832cb54ea41286ded8fc3",
+    "url": "/Cloverbook/fonts/fa-light-300.a624ea3f.eot"
+  },
+  {
+    "revision": "3e332211899360ab9380df4d50e1c33a",
+    "url": "/Cloverbook/fonts/fa-regular-400.3e332211.woff2"
+  },
+  {
+    "revision": "4776aa52b579295cf651f6284793dac4",
+    "url": "/Cloverbook/fonts/fa-regular-400.4776aa52.woff"
+  },
+  {
+    "revision": "faa0b37d4fdba1e304982f192c55bbe2",
+    "url": "/Cloverbook/fonts/fa-regular-400.faa0b37d.ttf"
+  },
+  {
+    "revision": "fde04f8e6967b818c6fb3338d8b366d3",
+    "url": "/Cloverbook/fonts/fa-regular-400.fde04f8e.eot"
+  },
+  {
+    "revision": "150120b3d9b448dd116680b66eba9bbe",
+    "url": "/Cloverbook/fonts/fa-solid-900.150120b3.woff2"
+  },
+  {
+    "revision": "6c207a7b79c06c76e915eb8f30e51d8a",
+    "url": "/Cloverbook/fonts/fa-solid-900.6c207a7b.eot"
+  },
+  {
+    "revision": "749dd3651c1a97f03faa88723af2371a",
+    "url": "/Cloverbook/fonts/fa-solid-900.749dd365.ttf"
+  },
+  {
+    "revision": "fb2f453136d024f47f2463d0d2ea2f05",
+    "url": "/Cloverbook/fonts/fa-solid-900.fb2f4531.woff"
+  },
+  {
+    "revision": "379b1f83cbda1e4ad6f70ebb5483de45",
+    "url": "/Cloverbook/img/b1.379b1f83.png"
+  },
+  {
+    "revision": "dc8254d9abf8bc1034c5d517c1371df4",
+    "url": "/Cloverbook/img/b1.dc8254d9.png"
+  },
+  {
+    "revision": "1cd0fb9d702340e31a1f701f7d80c378",
+    "url": "/Cloverbook/img/b2.1cd0fb9d.png"
+  },
+  {
+    "revision": "d9f8e683fd24aa697e13f93444121a36",
+    "url": "/Cloverbook/img/b2.d9f8e683.png"
+  },
+  {
+    "revision": "42eb790c3ca535f75c01d9391b2b4548",
+    "url": "/Cloverbook/img/b3.42eb790c.png"
+  },
+  {
+    "revision": "548bf8003dc53fbd765f26ab967efdba",
+    "url": "/Cloverbook/img/b3.548bf800.png"
+  },
+  {
+    "revision": "28d60409973f38208ceb29c5754c98d7",
+    "url": "/Cloverbook/img/b4.28d60409.png"
+  },
+  {
+    "revision": "bee8eb8d553ec907bdf68f5d7115c0cc",
+    "url": "/Cloverbook/img/b4.bee8eb8d.png"
+  },
+  {
+    "revision": "a8b17259fefd4233bac80110a464760d",
+    "url": "/Cloverbook/img/crime.a8b17259.png"
+  },
+  {
+    "revision": "6c6d2066f4cf11ef750989386713bccd",
+    "url": "/Cloverbook/img/fa-brands-400.6c6d2066.svg"
+  },
+  {
+    "revision": "bd556a0aeb87d75e1425ef5443c630a6",
+    "url": "/Cloverbook/img/fa-duotone-900.bd556a0a.svg"
+  },
+  {
+    "revision": "a83fcc84a08516a0e8bf77b2d982827a",
+    "url": "/Cloverbook/img/fa-light-300.a83fcc84.svg"
+  },
+  {
+    "revision": "a7fe14be02125376c03aced162443080",
+    "url": "/Cloverbook/img/fa-regular-400.a7fe14be.svg"
+  },
+  {
+    "revision": "29fd9e8a4f8962b6a48328553db58f2c",
+    "url": "/Cloverbook/img/fa-solid-900.29fd9e8a.svg"
+  },
+  {
+    "revision": "88242f2ab10f2f25e8fc5e7943577ca2",
+    "url": "/Cloverbook/img/m1.88242f2a.png"
+  },
+  {
+    "revision": "44db2eb5f0395f57b7d631497f392cf0",
+    "url": "/Cloverbook/img/m2.44db2eb5.png"
+  },
+  {
+    "revision": "d9522d29ff2b1388381720a194d7e4ec",
+    "url": "/Cloverbook/img/m3.d9522d29.png"
+  },
+  {
+    "revision": "ab6ebdeadcdc04a1fde336554ee01bd0",
+    "url": "/Cloverbook/img/m4.ab6ebdea.png"
+  },
+  {
+    "revision": "2f6fefa17cea6952e546488749ff3dc8",
+    "url": "/Cloverbook/img/slider-img-1.2f6fefa1.png"
+  },
+  {
+    "revision": "44763aad629b282f48910368bb5df255",
+    "url": "/Cloverbook/img/slider-img-2.44763aad.png"
+  },
+  {
+    "revision": "bd7281ffd7069b8476920e4ad95ca972",
+    "url": "/Cloverbook/img/slider-img-3.bd7281ff.png"
+  },
+  {
+    "revision": "6e7f5ef28610e727ee4bbfe57403938d",
+    "url": "/Cloverbook/index.html"
+  },
+  {
+    "revision": "4486887e6058026b460c",
+    "url": "/Cloverbook/js/app.48f7bd59.js"
+  },
+  {
+    "revision": "e70cc9a0a2e986231980",
+    "url": "/Cloverbook/js/chunk-vendors.4b34426d.js"
+  },
+  {
+    "revision": "c15df2071630d88854b0391d258c7e0a",
+    "url": "/Cloverbook/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Cloverbook/robots.txt"
+  }
+]);
